c82a18f62998dde4148484e0aa82e34ccaded09a
Nico Zeilfelder <nico.zeilfelder@codeagle.de>
2023-08-09 07:19:58 +0200

    bold pipe

--- src/app/shared/pipes/bold.pipe.ts
+++ src/app/shared/pipes/bold.pipe.ts
@@ -5,6 +5,8 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 export class BoldPipe implements PipeTransform {
     transform(value: string, searchTerm: string): unknown {
-        return value?.toLowerCase().replace(searchTerm.toLowerCase(), '<strong>' + searchTerm.toLowerCase() + '</strong>');
+        // return value?.toLowerCase().replace(searchTerm.toLowerCase(), '<strong>' + searchTerm.toLowerCase() + '</strong>');
+        return value?.toLowerCase().replace(new RegExp(searchTerm.toLowerCase(), 'gi'),
+            (match: string) => `<strong>${match}</strong>`);
     }
 }
